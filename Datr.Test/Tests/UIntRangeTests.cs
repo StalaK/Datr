774b365cb20612abd3a0406869d35c78d0e4d8d3
Josh Hughes <Josh.Hughes@coxauto.co.uk>
2020-09-26 17:02:43 +0100

    Add uint range tests

---
+++ Datr.Test/Tests/UIntRangeTests.cs
@@ -0,0 +1,161 @@
+﻿using Datr.Test.Objects;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Datr.Test.Tests
+{
+    [TestClass]
+    public class UIntRangeTests
+    {
+        [TestMethod]
+        public void AddUIntRangeToList()
+        {
+            var datr = new Datr();
+            datr.SetUIntRange<BasicClass>("UInt", Range.GreaterThan, 100U);
+
+            Assert.AreEqual(1, datr.FixedRanges.Count);
+            Assert.AreEqual(typeof(uint), datr.FixedRanges[0].DataType);
+            Assert.AreEqual(typeof(BasicClass), datr.FixedRanges[0].ClassType);
+            Assert.AreEqual(Range.GreaterThan, datr.FixedRanges[0].Range);
+            Assert.AreEqual(100U, datr.FixedRanges[0].MinValue);
+            Assert.IsNull(datr.FixedRanges[0].MaxValue);
+        }
+
+        [TestMethod]
+        public void UIntRangeLessThan()
+        {
+            var datr = new Datr();
+            datr.SetUIntRange<BasicClass>("UInt", Range.LessThan, maxValue: 100U);
+
+            for (int i = 0; i < 100; i++)
+            {
+                var basicClass = datr.Create<BasicClass>();
+                Assert.IsTrue(basicClass.UInt <= 100U, $"Value generated is {basicClass.UInt}");
+            }
+        }
+
+        [TestMethod]
+        public void UIntRangeGreaterThan()
+        {
+            var datr = new Datr();
+            datr.SetUIntRange<BasicClass>("UInt", Range.GreaterThan, 100U);
+
+            for (int i = 0; i < 100; i++)
+            {
+                var basicClass = datr.Create<BasicClass>();
+                Assert.IsTrue(basicClass.UInt >= 100U, $"Value generated is {basicClass.UInt}");
+            }
+        }
+
+        [TestMethod]
+        public void UIntRangeBetween()
+        {
+            var datr = new Datr();
+            datr.SetUIntRange<BasicClass>("UInt", Range.Between, 50U, 500U);
+
+            for (int i = 0; i < 100; i++)
+            {
+                var basicClass = datr.Create<BasicClass>();
+                Assert.IsTrue(basicClass.UInt >= 50U, $"Value generated is {basicClass.UInt}");
+                Assert.IsTrue(basicClass.UInt <= 500U, $"Value generated is {basicClass.UInt}");
+            }
+        }
+
+        [TestMethod]
+        public void UIntRangeOutside()
+        {
+            var datr = new Datr();
+            datr.SetUIntRange<BasicClass>("UInt", Range.Outside, 50U, 500U);
+
+            for (int i = 0; i < 100; i++)
+            {
+                var basicClass = datr.Create<BasicClass>();
+                Assert.IsTrue(basicClass.UInt < 50U || basicClass.UInt >= 500U, $"Value generated is {basicClass.UInt}");
+            }
+        }
+
+        [TestMethod]
+        public void UIntRangeMinValueNullBetweenRange()
+        {
+            var datr = new Datr();
+            Assert.ThrowsException<ArgumentException>(() => datr.SetUIntRange<BasicClass>("UInt", Range.Between, maxValue: 100U));
+        }
+
+        [TestMethod]
+        public void UIntRangeMinValueNullOutsideRange()
+        {
+            var datr = new Datr();
+            Assert.ThrowsException<ArgumentException>(() => datr.SetUIntRange<BasicClass>("UInt", Range.Outside, maxValue: 100U));
+        }
+
+        [TestMethod]
+        public void UIntRangeMaxValueNullBetweenRange()
+        {
+            var datr = new Datr();
+            Assert.ThrowsException<ArgumentException>(() => datr.SetUIntRange<BasicClass>("UInt", Range.Between, minValue: 100U));
+        }
+
+        [TestMethod]
+        public void UIntRangeMaxValueNullOutsideRange()
+        {
+            var datr = new Datr();
+            Assert.ThrowsException<ArgumentException>(() => datr.SetUIntRange<BasicClass>("UInt", Range.Outside, minValue: 100U));
+        }
+
+        [TestMethod]
+        public void IntRangeMinValueNullGreaterThanRange()
+        {
+            var datr = new Datr();
+            Assert.ThrowsException<ArgumentException>(() => datr.SetUIntRange<BasicClass>("UInt", Range.GreaterThan, maxValue: 100));
+        }
+
+        [TestMethod]
+        public void UIntRangeMaxValueNullLessThanRange()
+        {
+            var datr = new Datr();
+            Assert.ThrowsException<ArgumentException>(() => datr.SetUIntRange<BasicClass>("UInt", Range.LessThan, minValue: 100));
+        }
+
+        [TestMethod]
+        public void UIntRangeMaxValueEqualMinValueBetweenRange()
+        {
+            var datr = new Datr();
+            Assert.ThrowsException<ArgumentException>(() => datr.SetUIntRange<BasicClass>("UInt", Range.Between, minValue: 100, maxValue: 100));
+        }
+
+        [TestMethod]
+        public void UIntRangeMaxValueEqualMinValueOutsideRange()
+        {
+            var datr = new Datr();
+            Assert.ThrowsException<ArgumentException>(() => datr.SetUIntRange<BasicClass>("UInt", Range.Between, minValue: 100, maxValue: 100));
+        }
+
+        [TestMethod]
+        public void UIntRangeMaxValueLessThanMinValueBetweenRange()
+        {
+            var datr = new Datr();
+            Assert.ThrowsException<ArgumentException>(() => datr.SetUIntRange<BasicClass>("UInt", Range.Between, minValue: 100, maxValue: 90));
+        }
+
+        [TestMethod]
+        public void UIntRangeMaxValueLessThanMinValueOutsideRange()
+        {
+            var datr = new Datr();
+            Assert.ThrowsException<ArgumentException>(() => datr.SetUIntRange<BasicClass>("UInt", Range.Between, minValue: 100, maxValue: 90));
+        }
+
+        [TestMethod]
+        public void UIntRangeMinValueEqualsUIntMax()
+        {
+            var datr = new Datr();
+            Assert.ThrowsException<ArgumentException>(() => datr.SetUIntRange<BasicClass>("UInt", Range.GreaterThan, minValue: uint.MaxValue));
+        }
+
+        [TestMethod]
+        public void UIntRangeMaxValueEqualsUIntMin()
+        {
+            var datr = new Datr();
+            Assert.ThrowsException<ArgumentException>(() => datr.SetUIntRange<BasicClass>("UInt", Range.LessThan, maxValue: uint.MinValue));
+        }
+    }
+}
